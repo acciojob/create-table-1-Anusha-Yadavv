c918a226f79cda8edf4ccc8da1c53a366efaa4ac
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-21 12:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
+	let tableElement=document.getElementById("sampleTable");
+		let trElement=document.createElement("tr");
+
+	let tdElement=document.createElement("td");
+	tdElement.innerText="Row1 cell1";
+	tdElement.appendChild(trElement);
+	trElement.appendChild(tableElement);
+	
+
+	
   
   
 }
